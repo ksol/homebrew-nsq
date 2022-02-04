cbbcb34307b788f80cfeb83831e786e899ec78a2
Kevin Soltysiak <kevin@ksol.fr>
2022-02-04 10:25:16 +0100

    NSQ: align with upstream files

--- Formula/nsqlookup.rb
+++ Formula/nsqlookup.rb
@@ -1,29 +1,28 @@
 class Nsqlookup < Formula
   desc "Realtime distributed messaging platform"
   homepage "https://nsq.io/"
-  url "https://github.com/nsqio/nsq/archive/v1.1.0.tar.gz"
-  sha256 "85cb15cc9a7b50e779bc8e76309cff9bf555b2f925c2c8abe81d28d690fb1940"
-  head "https://github.com/nsqio/nsq.git"
+  url "https://github.com/nsqio/nsq/archive/v1.2.1.tar.gz"
+  sha256 "5fd252be4e9bf5bc0962e5b67ef5ec840895e73b1748fd0c1610fa4950cb9ee1"
+  license "MIT"
+  head "https://github.com/nsqio/nsq.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, mojave:      "2fa867147f43fa78509dc153f725c5e3325a1a268500811db2dfe5d90db6b823"
-    sha256 cellar: :any_skip_relocation, high_sierra: "ff5ee1076510935d467a359172c57a487f5c4fa50537c28643f60b6254d60348"
-    sha256 cellar: :any_skip_relocation, sierra:      "e06823b1c505fff73402522d13a74f386106987c96c83dcccb0b8e68e169449a"
-    sha256 cellar: :any_skip_relocation, el_capitan:  "02225f180c8e5ebc9d5e0ecd30c0875738b10904143c031a07709a6661362243"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "00f88216761ea1bed39c94c68c67acf66fccc895e0b212e44867b86be41bfe40"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "30543659d3c5990aa86f08346feea09adc2c7f5a388418f0c8eaff486cf388ec"
+    sha256 cellar: :any_skip_relocation, monterey:       "269501de2272e478c383ad7d71cee76c20c5191ff1f5cb25a69bf51364f5a980"
+    sha256 cellar: :any_skip_relocation, big_sur:        "daaf9729cff4ae0e02895d2c1fd398c592ee89f25b195be732c71a4b33f4a617"
+    sha256 cellar: :any_skip_relocation, catalina:       "533d1087999114a2a426dd589f4417d2b8cdb5a11b399a6004bca83f572aeb50"
+    sha256 cellar: :any_skip_relocation, mojave:         "154ac16069cd16a07ae6ca9ae2e432b26f52b2f2e79b5041322cbd726f3d7462"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "53915ff129d853cb2b5622889c938a7fd5ce073b90b8bdea4dc503d84b336e15"
   end
 
-  depends_on "dep" => :build
   depends_on "go" => :build
-  depends_on "btubbs/nsq/nsq"
+  depends_on "ksol/nsq/nsq"
 
   def install
-    ENV["GOPATH"] = buildpath
-    (buildpath/"src/github.com/nsqio/nsq").install buildpath.children
-    cd "src/github.com/nsqio/nsq" do
-      system "dep", "ensure", "--vendor-only"
-      system "make", "DESTDIR=#{prefix}", "PREFIX=", "install"
-      prefix.install_metafiles
-    end
+    system "make", "DESTDIR=#{prefix}", "PREFIX=", "install"
+    prefix.install_metafiles
   end
 
   def post_install
@@ -31,67 +30,46 @@ class Nsqlookup < Formula
     (var/"nsq").mkpath
   end
 
-  plist_options manual: "nsqlookupd"
-
-  def plist
-    <<~EOS
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-      <dict>
-        <key>KeepAlive</key>
-        <true/>
-        <key>Label</key>
-        <string>#{plist_name}</string>
-        <key>ProgramArguments</key>
-        <array>
-          <string>#{bin}/nsqlookupd</string>
-        </array>
-        <key>RunAtLoad</key>
-        <true/>
-        <key>WorkingDirectory</key>
-        <string>#{var}/nsq</string>
-        <key>StandardErrorPath</key>
-        <string>#{var}/log/nsqlookupd.error.log</string>
-        <key>StandardOutPath</key>
-        <string>#{var}/log/nsqlookupd.log</string>
-      </dict>
-      </plist>
-    EOS
+  service do
+    run [bin/"nsqlookupd"]
+    keep_alive true
+    working_dir var/"nsq"
+    log_path var/"log/nsqlookupd.log"
+    error_log_path var/"log/nsqlookupd.error.log"
   end
 
   test do
-      lookupd = fork do
-        exec bin/"nsqlookupd"
-      end
-      sleep 2
-      d = fork do
-        exec bin/"nsqd", "--lookupd-tcp-address=127.0.0.1:4160"
-      end
-      sleep 2
-      admin = fork do
-        exec bin/"nsqadmin", "--lookupd-http-address=127.0.0.1:4161"
-      end
-      sleep 2
-      to_file = fork do
-        exec bin/"nsq_to_file", "--lookupd-http-address=127.0.0.1:4161",
-                                "--output-dir=#{testpath}",
-                                "--topic=test"
-      end
-      sleep 2
-      system "curl", "-d", "hello", "http://127.0.0.1:4151/pub?topic=test"
-      sleep 2
-      dat = File.read(Dir["*.dat"].first)
-      assert_match "test", dat
-      assert_match version.to_s, dat
-    ensure
-      Process.kill(9, lookupd)
-      Process.kill(9, d)
-      Process.kill(9, admin)
-      Process.kill(9, to_file)
-      Process.wait lookupd
-      Process.wait d
-      Process.wait admin
-      Process.wait to_file
+    lookupd = fork do
+      exec bin/"nsqlookupd"
+    end
+    sleep 2
+    d = fork do
+      exec bin/"nsqd", "--lookupd-tcp-address=127.0.0.1:4160"
+    end
+    sleep 2
+    admin = fork do
+      exec bin/"nsqadmin", "--lookupd-http-address=127.0.0.1:4161"
+    end
+    sleep 2
+    to_file = fork do
+      exec bin/"nsq_to_file", "--lookupd-http-address=127.0.0.1:4161",
+                              "--output-dir=#{testpath}",
+                              "--topic=test"
+    end
+    sleep 2
+    system "curl", "-d", "hello", "http://127.0.0.1:4151/pub?topic=test"
+    sleep 2
+    dat = File.read(Dir["*.dat"].first)
+    assert_match "test", dat
+    assert_match version.to_s, dat
+  ensure
+    Process.kill(15, lookupd)
+    Process.kill(15, d)
+    Process.kill(15, admin)
+    Process.kill(15, to_file)
+    Process.wait lookupd
+    Process.wait d
+    Process.wait admin
+    Process.wait to_file
   end
 end
